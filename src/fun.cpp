8b9fb4de5a7682507e52e4ae1c1d36cce407a7c7
Tima-bd <100956970+Tima-bd@users.noreply.github.com>
2022-03-12 22:33:41 +0300

    Тимофей Баландин

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,32 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    int i = 0;
-    int now_word = 0;
-    int how_many_words = 0;
-    while (str[i] != ' ')
-    {
-        if (str[i] == ' ')
-        {
-            now_word = 0;
-        }
-        else if (str[i] != ' ' && now_word == 0 && isdigit(str[i]))
-        {
-            now_word = 1;
-            how_many_words++;
-        }
-        i++;
-    }
-    return how_many_words;
-}
-
-unsigned int faStr2(const char *str) {
+unsigned int faStr1(const char* str) {
     int i = 0;
     int now_word = 0;
     int how_many_words = 0;
     int good_word = 0;
-    while (str[i] != ' ')
+    while (str[i] != '\0')
     {
         if (str[i] == ' ' && good_word == 1)
         {
@@ -38,7 +18,42 @@ unsigned int faStr2(const char *str) {
         {
             now_word = 0;
         }
-        else if (str[i] != ' ' && now_word == 0 && str[i]>='A' && str[i]<='Z')
+        else if (str[i] != ' ' && now_word == 0 && isdigit(str[i]) == 0)
+        {
+            now_word = 1;
+            good_word = 1;
+        }
+        else if (str[i] != ' ' && isdigit(str[i]))
+        {
+            now_word = 1;
+            good_word = 0;
+        }
+
+
+        i++;
+    }
+    return how_many_words;
+}
+
+unsigned int faStr2(const char* str) {
+    int i = 0;
+    int now_word = 0;
+    int how_many_words = 0;
+    int good_word = 0;
+    while (str[i] != '\0')
+    {
+
+        if (str[i] == ' ' && good_word == 1)
+        {
+            how_many_words++;
+            good_word = 0;
+            now_word = 0;
+        }
+        else if (str[i] == ' ')
+        {
+            now_word = 0;
+        }
+        else if (str[i] != ' ' && now_word == 0 && str[i] >= 'A' && str[i] <= 'Z')
         {
             now_word = 1;
             good_word = 1;
@@ -58,12 +73,12 @@ unsigned int faStr2(const char *str) {
     return how_many_words;
 }
 
-unsigned int faStr3(const char *str) {
+unsigned int faStr3(const char* str) {
     int i = 0;
     int now_word = 0;
     int how_many_words = 0;
     int len = 0;
-    while (str[i] != ' ')
+    while (str[i] != '\0')
     {
         if (str[i] == ' ')
         {
