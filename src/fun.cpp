dc166e08dc3ad8ff6893133bbd8feb38f1638ae9
Tima-bd <100956970+Tima-bd@users.noreply.github.com>
2022-03-12 12:40:37 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,23 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+     int i = 0;
+    int now_word = 0;
+    int how_many_words = 0;
+    while (str[i] != ' ')
+    {
+        if (str[i] == ' ')
+        {
+            now_word = 0;
+        }
+        else if (str[i] != ' ' && now_word == 0 && isdigit(str[i]))
+        {
+            now_word = 1;
+            how_many_words++;
+        }
+        i++;
+    }
+    return how_many_words;
 }
 
 unsigned int faStr2(const char *str) {
